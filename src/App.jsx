4fea79683886be5a8c82919c4fe03e0b7f5bfd6a
erickgcia <erick17a@hotmail.com>
2024-05-22 16:54:19 -0600

    add: design mockup images

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/all'
-import { landingMockup, landingMockup2, landingMockup3 } from './assets/images'
+import {
+  designMockup1,
+  designMockup2,
+  designMockup3,
+  designMockup4,
+  designMockup5,
+  designMockup6,
+  designMockup7,
+  landingMockup,
+  landingMockup2,
+  landingMockup3,
+} from './assets/images'
 import { icons, quotes } from './assets/icons'
 gsap.registerPlugin(ScrollTrigger)
 
@@ -279,13 +290,55 @@ const App = () => {
               </button>
             </section>
             <section className="grid grid-cols-2 md:grid-cols-3 place-content-center gap-4 py-20">
-              <div className="w-full rounded-xl h-full col-span-1 md:col-span-2 t-bg"></div>
-              <div className="w-full rounded-xl aspect-square col-span-1 h-bg"></div>
-              <div className="w-full rounded-xl aspect-square col-span-1 h-bg"></div>
-              <div className="w-full rounded-xl aspect-square col-span-1 h-bg"></div>
-              <div className="w-full rounded-xl aspect-square col-span-1 h-bg"></div>
-              <div className="w-full rounded-xl col-span-1 md:col-span-2 t-bg"></div>
-              <div className="w-full rounded-xl aspect-square col-span-1 h-bg"></div>
+              <div className="w-full rounded-3xl h-full col-span-1 md:col-span-2 t-bg">
+                <img
+                  className="w-full h-full rounded-3xl md:rounded-none object-cover"
+                  src={designMockup1}
+                  alt="Design webiste mockup image"
+                />
+              </div>
+              <div className="w-full h-full rounded-3xl aspect-square col-span-1 b-bg">
+                <img
+                  className="w-full h-full rounded-3xl object-contain object-center"
+                  src={designMockup2}
+                  alt="Design webiste mockup image"
+                />
+              </div>
+              <div className="w-full rounded-3xl aspect-square col-span-1 h-bg flex place-content-center">
+                <img
+                  className="w-3/4 h-full object-cover object-top"
+                  src={designMockup3}
+                  alt="Design webiste mockup image"
+                />
+              </div>
+              <div className="w-full rounded-3xl aspect-square col-span-1 h-bg">
+                <img
+                  className="w-full h-full rounded-3xl object-cover object-top"
+                  src={designMockup4}
+                  alt="Design webiste mockup image"
+                />
+              </div>
+              <div className="w-full rounded-3xl aspect-square col-span-1 green-bg flex place-content-center">
+                <img
+                  className="w-3/4 h-full object-cover object-top"
+                  src={designMockup5}
+                  alt="Design webiste mockup image"
+                />
+              </div>
+              <div className="w-full md:h-3/4 rounded-3xl col-span-1 md:col-span-2 h-bg">
+                <img
+                  className="w-full rounded-3xl h-full object-cover object-center"
+                  src={designMockup6}
+                  alt="Design webiste mockup image"
+                />
+              </div>
+              <div className="w-full rounded-3xl aspect-square col-span-1">
+                <img
+                  className="w-full h-full rounded-3xl object-cover object-center"
+                  src={designMockup7}
+                  alt="Design webiste mockup image"
+                />
+              </div>
             </section>
           </article>
         </article>
--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,23 @@
 import landingMockup from './landing-mockup-1.png'
 import landingMockup2 from './landing-mockup-2.png'
 import landingMockup3 from './landing-mockup-3.png'
+import designMockup1 from './design-mockup-1.webp'
+import designMockup2 from './design-mockup-2.webp'
+import designMockup3 from './design-mockup-3.webp'
+import designMockup4 from './design-mockup-4.webp'
+import designMockup5 from './design-mockup-5.webp'
+import designMockup6 from './design-mockup-6.webp'
+import designMockup7 from './design-mockup-7.jpg'
 
-export { landingMockup, landingMockup2, landingMockup3 }
+export {
+  landingMockup,
+  landingMockup2,
+  landingMockup3,
+  designMockup1,
+  designMockup2,
+  designMockup3,
+  designMockup4,
+  designMockup5,
+  designMockup6,
+  designMockup7,
+}
