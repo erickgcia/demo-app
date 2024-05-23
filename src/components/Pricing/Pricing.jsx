00a733a461c87d376ddee78d17373fbe8aa1ef7f
erickgcia <erick17a@hotmail.com>
2024-05-23 12:32:58 -0600

    add: Plans pricing cards.

--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -3,8 +3,8 @@ const Pricing = () => {
     <article className="container-size">
       <section className="flex flex-col items-center justify-center gap-6 py-10">
         <div className="flex flex-col gap-2 font-medium">
-          <span className="text-2xl text-gray-400">Plans</span>
-          <strong className="text-5xl font-semibold">
+          <span className="text-3xl text-gray-400">Plans</span>
+          <strong className="text-6xl font-semibold">
             Choose what&apos;s right for you
           </strong>
         </div>
@@ -13,6 +13,11 @@ const Pricing = () => {
           design and no-code development service at a flat fee.
         </p>
       </section>
+      <article className="grid grid-cols-3 gap-4">
+        <section className="cols-span-1 w-full aspect-square rounded-3xl bg-custom-gray"></section>
+        <section className="cols-span-1 w-full aspect-square rounded-3xl h-bg"></section>
+        <section className="cols-span-1 w-full aspect-square rounded-3xl bg-custom-gray"></section>
+      </article>
     </article>
   )
 }
